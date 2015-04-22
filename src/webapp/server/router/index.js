e3171d26b773e98c717eecb42afee50c1183c4bb
Noah Spitzer-Williams <noah@spitzer-williams.com>
2015-04-21 22:14:19 -0700

    organized routes

--- src/webapp/server/router/index.js
+++ src/webapp/server/router/index.js
@@ -1,9 +1,7 @@
-var express = require('express');
-var router = express.Router();
+/**
+ * The Index of Routes
+ */
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-module.exports = router;
+module.exports = function (app) {
+  app.use('/users', require('./routes/users'));
+}
