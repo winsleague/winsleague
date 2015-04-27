7e23002918236a162541323a1d540036be9b0f3d
Noah Spitzer-Williams <noah@spitzer-williams.com>
2015-04-26 20:36:42 -0700

    created users model and updated /users/ resource

--- src/webapp/server/router/index.js
+++ src/webapp/server/router/index.js
@@ -1,3 +1,5 @@
+"use strict";
+
 /**
  * The Index of Routes
  */
--- src/webapp/server/router/routes/users.js
+++ src/webapp/server/router/routes/users.js
@@ -1,9 +1,12 @@
+var models = require('../../models');
 var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  models.User.findAll().then(function(users) {
+    res.send(users);
+  });
 });
 
 module.exports = router;
