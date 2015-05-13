d544e0fec179e0bacef953b19678d1926ec084e7
Noah Spitzer-Williams <noah@spitzer-williams.com>
2015-05-13 16:44:41 -0700

    use strict and indenting

--- src/webapp/server/app.js
+++ src/webapp/server/app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -27,39 +29,38 @@ var router = require('./router')(app);
  * Development Settings
  */
 if (app.get('env') === 'development') {
-    // This will change in production since we'll be using the dist folder
-    app.use(express.static(path.join(__dirname, '../client')));
-    // This covers serving up the index page
-    app.use(express.static(path.join(__dirname, '../client/.tmp')));
-    app.use(express.static(path.join(__dirname, '../client/app')));
+  // This will change in production since we'll be using the dist folder
+  app.use(express.static(path.join(__dirname, '../client')));
+  // This covers serving up the index page
+  app.use(express.static(path.join(__dirname, '../client/.tmp')));
+  app.use(express.static(path.join(__dirname, '../client/app')));
 
-    // Error Handling
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+  // Error Handling
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
     });
+  });
 }
 
 /**
  * Production Settings
  */
 if (app.get('env') === 'production') {
+  // changes it to use the optimized version for production
+  app.use(express.static(path.join(__dirname, '/dist')));
 
-    // changes it to use the optimized version for production
-    app.use(express.static(path.join(__dirname, '/dist')));
-
-    // production error handler
-    // no stacktraces leaked to user
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: {}
-        });
+  // production error handler
+  // no stacktraces leaked to user
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: {}
     });
+  });
 }
 
 module.exports = app;
