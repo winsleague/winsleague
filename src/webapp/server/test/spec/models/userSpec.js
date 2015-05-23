322b5f45f0305e1a7abc232399e7dc94bf0799c2
Noah Spitzer-Williams <noah@spitzer-williams.com>
2015-05-22 19:10:51 -0500

    initial user spec

---
+++ src/webapp/server/test/spec/models/userSpec.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var user = require('../../../models/user');
+
+describe('User model', function() {
+  describe('Properties', function() {
+    it('has a username', function() {
+      // expect(user.username).toBeDefined();
+      expect(true).toBeTruthy();
+    });
+  });
+});
