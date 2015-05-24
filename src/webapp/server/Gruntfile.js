bd54da6752ba3d83aebb6a3ab4611ff39668ca9a
Noah Spitzer-Williams <noah@spitzer-williams.com>
2015-05-23 23:10:31 -0500

    removed karma since it doesn't work on server-side

--- src/webapp/server/Gruntfile.js
+++ src/webapp/server/Gruntfile.js
@@ -11,12 +11,6 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
-    karma: {
-      unit: {
-        configFile: 'test/karma.conf.js'
-      }
-    },
-
     'babel': {
       options: {
         sourceMap: true
