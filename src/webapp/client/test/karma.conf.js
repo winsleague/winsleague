adbee023a9034834fa4b17edd32dded7169f3264
Noah Spitzer-Williams <noah@spitzer-williams.com>
2015-11-08 20:18:11 -0800

    Have phantomjs exit if a ResourceError is encountered (useful if karma exits without killing phantom)

--- src/webapp/client/test/karma.conf.js
+++ src/webapp/client/test/karma.conf.js
@@ -56,6 +56,11 @@ module.exports = function(config) {
       'PhantomJS'
     ],
 
+    phantomjsLauncher: {
+      // Have phantomjs exit if a ResourceError is encountered (useful if karma exits without killing phantom)
+      exitOnResourceError: true
+    },
+
     // Continuous Integration mode
     // if true, it capture browsers, run tests and exit
     singleRun: false,
