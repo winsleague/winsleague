c38e9d08563548cdfaf5ba3bcb9305cb7769caff
Noah Spitzer-Williams <noah@spitzer-williams.com>
2015-12-21 22:45:40 -0800

    test routinely passes

--- src/webapp/tests/models/User.test.js
+++ src/webapp/tests/models/User.test.js
@@ -4,12 +4,19 @@ var sinon = require('sinon'),
 describe('The User Model', function () {
   describe('#Create()', function() {
     it('should save a user', function (done) {
-      User.create({
-        username: 'username',
-        email: 'user@domain.com',
-        password: 'password'
-      }).exec(function (err, user) {
-        done(err, user);
+      User.destroy({})
+      .then(function() {
+        User.create({
+          username: 'username',
+          email: 'user@domain.com',
+          password: 'password'
+        })
+        .then(function(user) {
+          done();
+        })
+        .catch(function(err) {
+          done(err);
+        });
       });
     });
   });
