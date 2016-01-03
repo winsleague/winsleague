cfc588ea4ab9490684c6f339244726afa7a67f48
Noah Spitzer-Williams <noah@spitzer-williams.com>
2016-01-03 13:45:29 -0800

    clean up syntax

--- app/server/modules/nfl_game_data.js
+++ app/server/modules/nfl_game_data.js
@@ -1,7 +1,7 @@
 let prettyjson = Meteor.npmRequire( 'prettyjson' );
 
 Modules.server.nflGameData = {
-  ingestSeasonData: function (year) {
+  ingestSeasonData(year) {
     let week = 1;
     let url = `http://www.nfl.com/ajax/scorestrip?season=${year}&seasonType=REG&week=${week}`;
 
@@ -10,34 +10,14 @@ Modules.server.nflGameData = {
     let xmlString = response.content;
     log.debug(`xml: ${xmlString}`);
 
-    let json = xml2js.parseStringSync(xmlString, {mergeAttrs: true, explicitArray: false});
+    let json = xml2js.parseStringSync(xmlString, { mergeAttrs: true, explicitArray: false });
     log.debug(`parsed json: ${prettyjson.render(json)}`);
 
     log.debug(`parsed json.ss.gms.g: ${prettyjson.render(json.ss.gms.g)}`);
     json.ss.gms.g.forEach(Modules.server.nflGameData.saveGame);
-
-    return "all done";
-
-    /*
-     try {
-     return yield ingestWeekData(year, 1);
-     // sails.log("success!", message);
-     } catch (err) {
-     sails.log("error!", err);
-     }
-     */
-
-    /*
-     let weeks = [];
-     for (var i = 1; i < 3; i++) weeks.push(i);
-     sails.log(`weeks: ${weeks}`);
-     Promise.map(weeks, function (week) {
-     sails.log(`return promise for ${year}/${week}`);
-     return ingestWeekData(year, week);
-     });
-     */
   },
-  saveGame: function (game) {
+
+  saveGame(game) {
     log.info(`game: ${game.eid}`);
   }
 };
