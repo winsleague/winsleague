4513fd3784b4d613902b6204cf88b5c091c4f97c
Noah Spitzer-Williams <noah@spitzer-williams.com>
2016-01-03 15:18:02 -0800

    Pools collection

---
+++ app/lib/collections/pools.js
@@ -0,0 +1,32 @@
+Pools = new Mongo.Collection('pools');
+
+
+if (Meteor.isServer) {
+  Pools.allow({
+    insert: function (userId, doc) {
+      return false;
+    },
+
+    update: function (userId, doc, fieldNames, modifier) {
+      return false;
+    },
+
+    remove: function (userId, doc) {
+      return false;
+    }
+  });
+
+  Pools.deny({
+    insert: function (userId, doc) {
+      return true;
+    },
+
+    update: function (userId, doc, fieldNames, modifier) {
+      return true;
+    },
+
+    remove: function (userId, doc) {
+      return true;
+    }
+  });
+}
