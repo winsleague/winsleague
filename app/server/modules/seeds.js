790975092e0bc7216c06f00751bb62374561754d
Noah Spitzer-Williams <noah@spitzer-williams.com>
2016-01-03 12:10:42 -0800

    updated seeds to erase existing data (this makes it easier for us to call it when in dev mode)

--- app/server/modules/seeds.js
+++ app/server/modules/seeds.js
@@ -1,7 +1,18 @@
 Modules.server.seeds = {
   createLeagues: () => {
+    Leagues.remove({});
+    LeagueTeams.remove({});
+    Seasons.remove({});
+
     let nflId = Leagues.insert({ name: "NFL" });
-    LeagueTeams.insert({ league: nflId, name: "New York Giants", abbreviation: "NYG" });
-    LeagueTeams.insert({ league: nflId, name: "Seattle Seahawks", abbreviation: "SEA" });
+    let teams = [
+      { league: nflId, name: "New York Giants", abbreviation: "NYG", conference: "NFC", division: "East" },
+      { league: nflId, name: "Seattle Seahawks", abbreviation: "SEA", conference: "NFC", division: "West" }
+    ];
+    for (var team of teams) {
+      LeagueTeams.insert(team);
+    }
+
+    Seasons.insert({ league: nflId, year: 2015 });
   }
 };
