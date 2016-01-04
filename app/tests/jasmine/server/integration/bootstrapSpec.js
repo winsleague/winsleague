80bd01dba9feec605137129c950603204a5e3c1f
Noah Spitzer-Williams <noah@spitzer-williams.com>
2016-01-03 18:12:23 -0800

    more specs

--- app/tests/jasmine/server/integration/bootstrapSpec.js
+++ app/tests/jasmine/server/integration/bootstrapSpec.js
@@ -1,5 +1,9 @@
 describe("Bootstrap", function () {
-  it ("should initialize the leagues", function () {
-    expect(Leagues.find().count()).toEqual(1);
+  it ("should seed the Leagues", function () {
+    expect(Leagues.find().count()).toBeGreaterThan(0);
+  });
+
+  it ("should seed the League Teams", function () {
+    expect(LeagueTeams.find().count()).toBeGreaterThan(0);
   });
 });
