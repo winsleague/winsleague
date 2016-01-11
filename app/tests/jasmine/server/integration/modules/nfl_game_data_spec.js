6f344a1f7c049147a66ddf83e67800b62c6d2c16
Noah Spitzer-Williams <noah@spitzer-williams.com>
2016-01-10 17:52:08 -0800

    more efficient specs, with reset database functionality

--- app/tests/jasmine/server/integration/modules/nfl_game_data_spec.js
+++ app/tests/jasmine/server/integration/modules/nfl_game_data_spec.js
@@ -1,16 +1,23 @@
 describe("NFL Game Data", function () {
+  beforeEach(function() {
+    const season = Modules.server.nflGameData.getSeason(2015);
+    const week = 3;
+    Modules.server.nflGameData.ingestWeekData(season, week);
+  });
+
+  afterEach(function() {
+    Games.remove({});
+    SeasonLeagueTeams.remove({});
+  });
+
   describe("Ingest Season Data", function () {
     it ("should ingest all games for the year", function () {
-      const season = Modules.server.nflGameData.getSeason(2015);
-      Modules.server.nflGameData.ingestSeasonData(season);
       expect(Games.find().count()).toBeGreaterThan(0);
     });
   });
 
   describe("Update Scores", function () {
     it ("should update the games based on live scores", function () {
-      const season = Modules.server.nflGameData.getSeason(2015);
-      Modules.server.nflGameData.ingestSeasonData(season);
       Modules.server.nflGameData.updateScores();
       expect(Games.find().count()).toBeGreaterThan(0);
     });
