7f7316703ab4f1e7e9c0ca45a626a7c144c6f25a
Noah Spitzer-Williams <noah@spitzer-williams.com>
2016-01-11 22:08:07 -0800

    fixed create button bug and got NFL league to select by default

--- app/client/templates/pools/pool_new.js
+++ app/client/templates/pools/pool_new.js
@@ -2,22 +2,25 @@ Template.poolNew.events({
 });
 
 Template.poolNew.helpers({
-  leagueOptions: function () {
+  leagueOptions: function() {
     return Leagues.find({}).map( function(league) { return { label: league.name, value: league._id } } );
+  },
+  nflLeagueId: function() {
+    return Leagues.findOne({ name: "NFL" })._id;
   }
 });
 
-Template.poolNew.onCreated(function () {
+Template.poolNew.onCreated(function() {
   var self = this;
   self.autorun(function() {
-    self.subscribe('leagues', function () {
+    self.subscribe('leagues', function() {
       log.info(`League data ready: ${Leagues.find().count()} leagues`);
     });
   });
 });
 
-Template.poolNew.onRendered(function () {
+Template.poolNew.onRendered(function() {
 });
 
-Template.poolNew.onDestroyed(function () {
+Template.poolNew.onDestroyed(function() {
 });
