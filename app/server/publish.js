ac754938839621e08c44f6ad9ad5ae1a019bdf5d
Noah Spitzer-Williams <noah@spitzer-williams.com>
2016-01-12 22:26:10 -0800

    check during publish

--- app/server/publish.js
+++ app/server/publish.js
@@ -1,7 +1,7 @@
 
 
 
-Meteor.publish('leaderboard', function () {
+Meteor.publish('leaderboard', function() {
   const pool = Pools.findOne({}, { fields: {_id: 1} });
   const users = PoolUserTeams.find({ poolId: pool._id });
   if (users) { return users; }
@@ -9,15 +9,16 @@ Meteor.publish('leaderboard', function () {
   return this.ready();
 });
 
-Meteor.publish('leagues', function () {
+Meteor.publish('leagues', function() {
   return Leagues.find({});
 });
 
-Meteor.publish('seasons', function () {
+Meteor.publish('seasons', function() {
   return Seasons.find({});
 });
 
 Meteor.publish('singlePool', function(id) {
+  check(id, String);
   const pool = Pools.find({ _id: id });
   const users = PoolUserTeams.find({ poolId: id });
   return [pool, users];
