368730ab1252ddc1d72ac28779dbf75ca8715e37
Noah Spitzer-Williams <noah@spitzer-williams.com>
2016-01-17 20:36:34 -0800

    removed leaderboard

--- app/server/publish.js
+++ app/server/publish.js
@@ -1,14 +1,3 @@
-
-
-
-Meteor.publish('leaderboard', function() {
-  const pool = Pools.findOne({}, { fields: {_id: 1} });
-  const users = PoolTeams.find({ poolId: pool._id });
-  if (users) { return users; }
-
-  return this.ready();
-});
-
 Meteor.publish('leagues', function() {
   return Leagues.find({});
 });
