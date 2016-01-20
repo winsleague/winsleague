6481481d1eaf6db76feb6e732864b6a88cd268d9
Noah Spitzer-Williams <noah@spitzer-williams.com>
2016-01-19 23:16:54 -0800

    styling

--- app/lib/collections/pool_teams.js
+++ app/lib/collections/pool_teams.js
@@ -4,76 +4,76 @@ PoolTeams.schema = new SimpleSchema({
   leagueId: {
     type: String,
     regEx: SimpleSchema.RegEx.Id,
-    autoValue: function() {
+    autoValue() {
       if (this.isInsert) {
-        return Pools.findOne({ _id: this.field("poolId").value }).leagueId;
+        return Pools.findOne(this.field('poolId').value).leagueId;
       }
-    }
+    },
   },
   seasonId: {
     type: String,
     regEx: SimpleSchema.RegEx.Id,
-    autoValue: function() {
+    autoValue() {
       if (this.isInsert) {
-        return Pools.findOne({ _id: this.field("poolId").value }).seasonId;
+        return Pools.findOne(this.field('poolId').value).seasonId;
       }
-    }
+    },
   },
   poolId: { type: String, regEx: SimpleSchema.RegEx.Id },
   userId: {
     type: String,
-    regEx: SimpleSchema.RegEx.Id
+    regEx: SimpleSchema.RegEx.Id,
   },
   userTeamName: {
-    label: "Team name",
-    type: String
+    label: 'Team name',
+    type: String,
   },
   leagueTeamIds: {
-    label: "Drafted teams",
+    label: 'Drafted teams',
     type: [String],
     regEx: SimpleSchema.RegEx.Id,
     autoform: {
       minCount: 1,
       maxCount: 4,
-      initialCount: 4
-    }
+      initialCount: 4,
+    },
   },
   'leagueTeamIds.$': {
     autoform: {
       afFieldInput: {
-        options: function() {
-          return LeagueTeams.find({}).map( function(leagueTeam) {
+        options() {
+          return LeagueTeams.find({}).map(leagueTeam => {
             return { label: leagueTeam.fullName(), value: leagueTeam._id };
-          } );
-        }
-      }
-    }
+          });
+        },
+      },
+    },
   },
   pickNumbers: {
     type: [Number],
-    autoValue: function() {
+    autoValue() {
       // TODO: this is placeholder until we wire this up
       if (this.isInsert) {
         let numbers = [];
-        for (var leagueTeamId of this.field("leagueTeamIds").value) {
+        for (let leagueTeamId of this.field('leagueTeamIds').value) {
           numbers.push(0);
         }
         return numbers;
       }
-    }
+    },
   },
   leagueTeamMascotNames: {
     type: [String],
-    autoValue: function() {
+    autoValue() {
       if (this.isInsert) {
         let mascots = [];
-        for (var leagueTeamId of this.field("leagueTeamIds").value) {
-          const leagueTeam = LeagueTeams.findOne({_id: leagueTeamId});
+        for (let leagueTeamId of this.field('leagueTeamIds').value) {
+          const leagueTeam = LeagueTeams.findOne(leagueTeamId);
           mascots.push(leagueTeam.mascotName);
         }
         return mascots;
       }
-    }
+    },
   },
   totalWins: { type: Number, defaultValue: 0 },
   totalGames: { type: Number, defaultValue: 0 },
@@ -82,69 +82,67 @@ PoolTeams.schema = new SimpleSchema({
     // Force value to be current date (on server) upon insert
     // and prevent updates thereafter.
     type: Date,
-    autoValue: function() {
+    autoValue() {
       if (this.isInsert) {
         return new Date();
       } else if (this.isUpsert) {
-        return {$setOnInsert: new Date()};
-      } else {
-        this.unset();  // Prevent user from supplying their own value
+        return { $setOnInsert: new Date() };
       }
-    }
+      this.unset();  // Prevent user from supplying their own value
+    },
   },
   updatedAt: {
     // Force value to be current date (on server) upon update
     // and don't allow it to be set upon insert.
     type: Date,
-    autoValue: function() {
+    autoValue() {
       if (this.isUpdate) {
         return new Date();
       }
     },
     denyInsert: true,
-    optional: true
-  }
+    optional: true,
+  },
 });
 PoolTeams.attachSchema(PoolTeams.schema);
 
 PoolTeams.helpers({
-  teamSummary: function () {
+  teamSummary() {
     let string = '';
-    for (var i = 0; i < this.leagueTeamMascotNames.length; i++) {
+    for (let i = 0; i < this.leagueTeamMascotNames.length; i++) {
       string += `${this.leagueTeamMascotNames[i]} #${this.pickNumbers[i]}, `;
     }
-    if (string.length > 0)
-      string = string.substr(0, string.length - 2);
+    if (string.length > 0) string = string.substr(0, string.length - 2);
     return string;
-  }
+  },
 });
 
 if (Meteor.isServer) {
   PoolTeams.allow({
-    insert: function (userId, doc) {
+    insert(userId, doc) {
       return false;
     },
 
-    update: function (userId, doc, fieldNames, modifier) {
+    update(userId, doc, fieldNames, modifier) {
       return false;
     },
 
-    remove: function (userId, doc) {
+    remove(userId, doc) {
       return false;
-    }
+    },
   });
 
   PoolTeams.deny({
-    insert: function (userId, doc) {
+    insert(userId, doc) {
       return true;
     },
 
-    update: function (userId, doc, fieldNames, modifier) {
+    update(userId, doc, fieldNames, modifier) {
       return true;
     },
 
-    remove: function (userId, doc) {
+    remove(userId, doc) {
       return true;
-    }
+    },
   });
 }
