583ef4a15af45ab4877c6c52ddd6bee87c60ef73
Noah Spitzer-Williams <noah@spitzer-williams.com>
2016-01-21 22:03:20 -0800

    fixed redirect

--- app/client/templates/pools/teams/pool_teams_new.js
+++ app/client/templates/pools/teams/pool_teams_new.js
@@ -3,28 +3,28 @@ Template.poolTeamsNew.events({
 
 PoolTeams.insertFormSchema = new SimpleSchema({
   poolId: { type: String, regEx: SimpleSchema.RegEx.Id },
-  userTeamName: { label: "Team name", type: String },
-  userEmail: { label: "Email", type: String, regEx: SimpleSchema.RegEx.Email },
+  userTeamName: { label: 'Team name', type: String },
+  userEmail: { label: 'Email', type: String, regEx: SimpleSchema.RegEx.Email },
   leagueTeamIds: {
-    label: "Drafted teams",
+    label: 'Drafted teams',
     type: [String],
     autoform: {
       minCount: 1,
       maxCount: 4,
-      initialCount: 4
-    }
+      initialCount: 4,
+    },
   },
   'leagueTeamIds.$': {
     autoform: {
       afFieldInput: {
-        options: function () {
-          return LeagueTeams.find({}, {sort: ["cityName", "asc"]}).map(function (leagueTeam) {
-            return {label: leagueTeam.fullName(), value: leagueTeam._id}
+        options() {
+          return LeagueTeams.find({}, { sort: ['cityName', 'asc'] }).map(function (leagueTeam) {
+            return { label: leagueTeam.fullName(), value: leagueTeam._id };
           });
-        }
-      }
-    }
-  }
+        },
+      },
+    },
+  },
 });
 
 Template.poolTeamsNew.helpers({
@@ -32,12 +32,12 @@ Template.poolTeamsNew.helpers({
   poolId: () => {
     const instance = Template.instance();
     return instance.getPoolId();
-  }
+  },
 });
 
 Template.poolTeamsNew.onCreated(function() {
   this.getPoolId = () => FlowRouter.getParam('poolId');
-  this.getLeagueId = () => Pools.findOne({ _id: this.getPoolId() }, { fields: { leagueId: 1 } }).leagueId;
+  this.getLeagueId = () => Pools.findOne(this.getPoolId(), { fields: { leagueId: 1 } }).leagueId;
 
   this.autorun(() => {
     this.subscribe('singlePool', this.getPoolId(), () => {
@@ -57,10 +57,9 @@ Template.poolTeamsNew.onDestroyed(function() {
 
 
 AutoForm.hooks({
-  insertPoolPlayerForm: {
-    onSuccess: (operation, poolTeamId) => {
-      // TODO: redirect to poolId, not poolTeamId
-      FlowRouter.go("poolsShow", { _id: poolTeamId });
-    }
-  }
+  insertPoolTeamForm: {
+    onSuccess: (operation, doc) => {
+      FlowRouter.go('poolsShow', { _id: FlowRouter.getParam('poolId') });
+    },
+  },
 });
