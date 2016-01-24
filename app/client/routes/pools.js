53daffe8d3f16bc77ada9763bda7128667de1f0a
Noah Spitzer-Williams <noah@spitzer-williams.com>
2016-01-23 16:54:40 -0800

    code styling

--- app/client/routes/pools.js
+++ app/client/routes/pools.js
@@ -1,43 +1,43 @@
 const poolsRoutes = FlowRouter.group({
-  prefix: '/pools'
+  prefix: '/pools',
 });
 
 // http://app.com/pools
-poolsRoutes.route( '/', {
-  action: function() {
-    console.log( "We're viewing a list of pools." );
-  }
+poolsRoutes.route('/', {
+  action() {
+    log.debug("We're viewing a list of pools.");
+  },
 });
 
 // http://app.com/pools/new
-poolsRoutes.route( '/new', {
-  name: "poolsNew",
-  action: function() {
-    BlazeLayout.render("masterLayout", { content: "poolsNew" });
-  }
+poolsRoutes.route('/new', {
+  name: 'poolsNew',
+  action() {
+    BlazeLayout.render('masterLayout', { content: 'poolsNew' });
+  },
 });
 
 // http://app.com/pools/:_id
-poolsRoutes.route( '/:_id', {
-  name: "poolsShow",
-  action: function(params) {
-    console.log(`We're viewing a single document: ${params._id}`);
-    BlazeLayout.render("masterLayout", { content: "poolsShow" });
-  }
+poolsRoutes.route('/:_id', {
+  name: 'poolsShow',
+  action(params) {
+    log.debug(`We're viewing a single document: ${params._id}`);
+    BlazeLayout.render('masterLayout', { content: 'poolsShow' });
+  },
 });
 
 // http://app.com/pools/:_id/edit
-poolsRoutes.route( '/:_id/edit', {
-  action: function(params) {
-    console.log(`We're editing a single document: ${params._id}`);
-  }
+poolsRoutes.route('/:_id/edit', {
+  action(params) {
+    log.debug(`We're editing a single document: ${params._id}`);
+  },
 });
 
 // http://app.com/pools/:_poolId/teams/new
-poolsRoutes.route( '/:poolId/teams/new', {
-  name: "poolTeamsNew",
-  action: function(params) {
-    console.log(`We're creating teams for a pool: ${params.poolId}`);
-    BlazeLayout.render("masterLayout", { content: "poolTeamsNew" });
-  }
+poolsRoutes.route('/:poolId/teams/new', {
+  name: 'poolTeamsNew',
+  action(params) {
+    log.debug(`We're creating teams for a pool: ${params.poolId}`);
+    BlazeLayout.render('masterLayout', { content: 'poolTeamsNew' });
+  },
 });
