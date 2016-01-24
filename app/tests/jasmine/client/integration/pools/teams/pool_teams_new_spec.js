7ee2804ef0065b9fae3ff02f2e0d00ab995c93f7
Noah Spitzer-Williams <noah@spitzer-williams.com>
2016-01-24 10:58:44 -0800

    increased delay even more for CI

--- app/tests/jasmine/client/integration/pools/teams/pool_teams_new_spec.js
+++ app/tests/jasmine/client/integration/pools/teams/pool_teams_new_spec.js
@@ -34,7 +34,7 @@ describe('poolTeamsNew page', () => {
       page.getFirstLeagueTeamField().find('option:eq(1)').prop('selected', true);
 
       $('form').submit();
-    }, DEFAULT_DELAY * 2);
+    }, DEFAULT_DELAY * 5);
 
     setTimeout(() => {
       const poolTeam = PoolTeams.findOne({ userTeamName });
@@ -44,6 +44,6 @@ describe('poolTeamsNew page', () => {
       expect(poolTeam.leagueTeamIds[0]).toBe(leagueTeamId);
 
       done();
-    }, DEFAULT_DELAY * 5);
+    }, DEFAULT_DELAY * 10);
   });
 });
