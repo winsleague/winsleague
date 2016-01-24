fb6f521058cfc10e76a823eb95df822b97604ea4
Noah Spitzer-Williams <noah@spitzer-williams.com>
2016-01-23 17:27:06 -0800

    spec passes!

--- app/tests/jasmine/client/integration/pools/teams/pool_teams_new_spec.js
+++ app/tests/jasmine/client/integration/pools/teams/pool_teams_new_spec.js
@@ -13,12 +13,16 @@ const page = {
 fdescribe('poolTeamsNew page', () => {
   beforeEach(loginWithDefaultUser);
   beforeEach(createDefaultPool);
-  beforeEach(goToPoolTeamsNewPage);
-  afterEach(logoutUser);
+  beforeEach(done => {
+    setTimeout(() => {
+      goToPoolTeamsNewPage(done);
+    }, DEFAULT_DELAY);
+  });
 
   it('should create new pool team', (done) => {
     const userEmail = 'dummy@dummy.com';
     const userTeamName = "Dummy's Dummies";
+    let leagueTeamId;
 
     setTimeout(() => {
       page.getUserEmailField().val(userEmail);
@@ -26,6 +30,7 @@ fdescribe('poolTeamsNew page', () => {
       page.getUserTeamNameField().val(userTeamName);
 
       // select first team
+      leagueTeamId = LeagueTeams.findOne({})._id;
       page.getFirstLeagueTeamField().find('option:eq(1)').prop('selected', true);
 
       $('form').submit();
@@ -36,9 +41,8 @@ fdescribe('poolTeamsNew page', () => {
       expect(poolTeam).not.toBe(undefined);
       log.info(`poolTeam: `, poolTeam);
       expect(poolTeam.userTeamName).toBe(userTeamName);
+      expect(poolTeam.leagueTeamIds[0]).toBe(leagueTeamId);
 
-      const newUser = Accounts.findUserByEmail(userEmail);
-      expect(poolTeam.userId).toBe(newUser._id);
       done();
     }, DEFAULT_DELAY * 2);
   });
