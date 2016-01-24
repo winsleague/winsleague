a299e429f32be360abbd6251854860a3a1bf28db
Noah Spitzer-Williams <noah@spitzer-williams.com>
2016-01-24 11:19:31 -0800

    trying to add timeout to home page to see if that fixes spec

--- app/tests/jasmine/client/integration/home/home_spec.js
+++ app/tests/jasmine/client/integration/home/home_spec.js
@@ -1,7 +1,10 @@
 describe('The homepage', () => {
   beforeEach(goToHomePage);
 
-  it('should have a title', () => {
-    expect($('h2').text()).toEqual('Simple Fantasy Sports');
+  it('should have a title', done => {
+    setTimeout(() => {
+      expect($('h2').text()).toEqual('Simple Fantasy Sports');
+      done();
+    }, DEFAULT_DELAY);
   });
 });
