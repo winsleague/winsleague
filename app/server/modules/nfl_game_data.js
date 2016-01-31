51dc07ef08541de18e016187df89bf8a15459225
Noah Spitzer-Williams <noah@spitzer-williams.com>
2016-01-30 21:41:51 -0800

    lint fixes

--- app/server/modules/nfl_game_data.js
+++ app/server/modules/nfl_game_data.js
@@ -1,21 +1,21 @@
-let prettyjson = Meteor.npmRequire( 'prettyjson' );
+const prettyjson = Meteor.npmRequire('prettyjson');
 
 Modules.server.nflGameData = {
   updateLiveScores() {
     const url = `http://www.nfl.com/liveupdate/scorestrip/scorestrip.json`;
     const response = HTTP.get(url);
     log.debug(`raw content: ${response.content}`);
-    let content = response.content.replace(/,,/g, ",\"\",");
-    content = content.replace(/,,/g, ",\"\","); // do it again to address multiple commas in a row
+    let content = response.content.replace(/,,/g, ',"",');
+    content = content.replace(/,,/g, ',"",'); // do it again to address multiple commas in a row
     log.debug(`fixed content: ${content}`);
     const json = JSON.parse(content);
     log.debug(`parsed json: ${prettyjson.render(json)}`);
 
     const league = Modules.server.nflGameData.getLeague();
 
-    for (let gameData of json.ss) {
+    for (const gameData of json.ss) {
       // ["Sun","13:00:00","Final",,"NYJ","17","BUF","22",,,"56744",,"REG17","2015"]
-      const gameId = parseInt(gameData[10]);
+      const gameId = parseInt(gameData[10], 10);
       const period = gameData[2].toLowerCase();
       const timeRemaining = gameData[3];
       const homeScore = gameData[7];
@@ -30,10 +30,10 @@ Modules.server.nflGameData = {
   },
 
   ingestSeasonData(season) {
-    if (season == null) { throw new Error(`Season is null!`); }
+    if (!season) { throw new Error(`Season is null!`); }
 
     const league = Modules.server.nflGameData.getLeague();
-    if (league == null) { throw new Error(`League is not found!`); }
+    if (!league) { throw new Error(`League is not found!`); }
 
     Games.remove({ leagueId: league._id, seasonId: season._id });
 
@@ -54,7 +54,7 @@ Modules.server.nflGameData = {
     log.debug(`parsed json: ${prettyjson.render(json)}`);
 
     log.debug(`parsed json.ss.gms.g: ${prettyjson.render(json.ss.gms.g)}`);
-    for (let game of json.ss.gms.g) {
+    for (const game of json.ss.gms.g) {
       Modules.server.nflGameData.saveGame(game, season, week);
     }
   },
@@ -67,38 +67,38 @@ Modules.server.nflGameData = {
       leagueId: league._id,
       seasonId: season._id,
       gameId: game.gsis,
-      gameDate: gameDate,
-      week: week,
+      gameDate,
+      week,
       homeTeamId: LeagueTeams.findOne({ leagueId: league._id, abbreviation: game.h })._id,
       homeScore: game.hs,
       awayTeamId: LeagueTeams.findOne({ leagueId: league._id, abbreviation: game.v })._id,
       awayScore: game.vs,
       period: Modules.server.nflGameData.cleanPeriod(game.q),
-      status: Modules.server.nflGameData.cleanStatus(game.q)
+      status: Modules.server.nflGameData.cleanStatus(game.q),
     });
   },
 
   getLeague() {
-    return Leagues.findOne({ name: "NFL" });
+    return Leagues.findOne({ name: 'NFL' });
   },
 
   getSeason(year = (new Date()).getFullYear()) {
     const league = Modules.server.nflGameData.getLeague();
-    if (league == null) { throw new Error(`NFL League not found!`); }
-    return Seasons.findOne({ leagueId: league._id, year })
+    if (!league) { throw new Error(`NFL League not found!`); }
+    return Seasons.findOne({ leagueId: league._id, year });
   },
 
   cleanPeriod(old) {
-    if (old == "P") { return "pregame"; }
-    if (old == "O") { return "overtime"; }
-    if (old == "F") { return "final"; }
-    if (old == "FO") { return "final overtime"; }
+    if (old === 'P') { return 'pregame'; }
+    if (old === 'O') { return 'overtime'; }
+    if (old === 'F') { return 'final'; }
+    if (old === 'FO') { return 'final overtime'; }
     return old;
   },
 
   cleanStatus(old) {
-    if (old == "P") { return "scheduled"; }
-    if (old == "F" || old == "FO") { return "completed"; }
-    return "in progress";
-  }
+    if (old === 'P') { return 'scheduled'; }
+    if (old === 'F' || old === 'FO') { return 'completed'; }
+    return 'in progress';
+  },
 };
