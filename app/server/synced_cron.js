785fef23dea5e440d59d10e5cbc50eae5be0cb0e
Noah Spitzer-Williams <noah@spitzer-williams.com>
2016-01-31 18:10:57 -0800

    changed cron to every 2am

--- app/server/synced_cron.js
+++ app/server/synced_cron.js
@@ -2,7 +2,7 @@ SyncedCron.add({
   name: 'Refresh NBA standings',
   schedule(parser) {
     // parser is a later.parse object
-    return parser.text('every 1 minute');
+    return parser.recur().on(2).hour(); // every 2am
   },
   job() {
     Modules.server.nbaGameData.ingestSeasonData();
