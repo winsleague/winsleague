384c00d9e25d61f7419ec56481b3cf7f808a5309
Noah Spitzer-Williams <noah@spitzer-williams.com>
2016-02-03 22:56:58 -0800

    remove logging of environment variables

--- app/server/bootstrap.js
+++ app/server/bootstrap.js
@@ -3,7 +3,6 @@ Meteor.startup(() => {
 
   // Note: this is not called when the test runner restarts due to a changed spec
   log.info(`Meteor.startup()`);
-  log.info(`process.env = `, process.env);
 
   if (Leagues.find().count() === 0) {
     Modules.server.seeds.createLeagues();
