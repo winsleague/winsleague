9e108efd58df2df4e222946f5f619721df09b6fd
Noah Spitzer-Williams <noah@spitzer-williams.com>
2016-02-09 08:20:16 -0800

    trying to fix loggly by fixing isProduction()

--- app/lib/modules/environment.js
+++ app/lib/modules/environment.js
@@ -1,9 +1,7 @@
 Modules.environment = {
   isProduction() {
-    if (Meteor.isClient) {
-      return Meteor.settings.public.METEOR_ENV && Meteor.settings.public.METEOR_ENV === 'production';
-    } else if (Meteor.isServer) {
-      return process.env.METEOR_ENV === 'production';
-    }
+    // we do it this way so it works on the client as well.
+    // the client doesn't have access to process.env
+    return Meteor.settings.public.METEOR_ENV && Meteor.settings.public.METEOR_ENV === 'production';
   },
 };
