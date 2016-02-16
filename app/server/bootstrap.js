e678b44f5278ad15701006bf2e5ffdd5dd031c25
Noah Spitzer-Williams <noah@spitzer-williams.com>
2016-02-15 21:34:43 -0800

    moved seeding to before migrations so the leagues are in place

--- app/server/bootstrap.js
+++ app/server/bootstrap.js
@@ -4,11 +4,8 @@ Meteor.startup(() => {
   // Note: this is not called when the test runner restarts due to a changed spec
   log.info(`Meteor.startup()`);
 
+  if (Leagues.find().count() === 0) Modules.server.seeds.createLeagues();
 
   Migrations.config({ logger: log });
   Migrations.migrateTo('latest');
-
-  if (Leagues.find().count() === 0) {
-    Modules.server.seeds.createLeagues();
-  }
 });
