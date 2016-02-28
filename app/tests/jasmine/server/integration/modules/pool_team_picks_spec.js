3136bef7599b46956faa453ca022f2f3b48922e9
Noah Spitzer-Williams <noah@spitzer-williams.com>
2016-02-28 12:07:06 -0800

    picks spec

---
+++ app/tests/jasmine/server/integration/modules/pool_team_picks_spec.js
@@ -0,0 +1,37 @@
+describe('Pool Team Picks', () => {
+  describe('updatePickQuality', () => {
+    beforeEach(Package.testing.Fixtures.pools.createDefaultPool);
+    beforeEach(Package.testing.Fixtures.poolTeams.createDefaultPoolTeam);
+
+    it('should update the pick quality ', () => {
+      const league = Modules.leagues.getByName('NFL');
+      const season = Modules.seasons.getByYear(league, 2015);
+      const leagueTeam = Modules.leagueTeams.getByName(league, 'New York', 'Giants');
+
+      const seasonLeagueTeamId = SeasonLeagueTeams.insert({
+        leagueId: league._id,
+        seasonId: season._id,
+        leagueTeamId: leagueTeam._id,
+        wins: 13,
+      });
+
+      const poolTeamId = PoolTeams.findOne()._id;
+      const poolTeamPickId = PoolTeamPicks.insert({
+        poolTeamId,
+        leagueTeamId: leagueTeam._id,
+        pickNumber: 8,
+      });
+
+      let poolTeamPick = PoolTeamPicks.findOne(poolTeamPickId);
+
+      Modules.server.poolTeamPicks.updatePickQuality(poolTeamPick);
+
+      poolTeamPick = PoolTeamPicks.findOne(poolTeamPickId);
+      log.debug('poolTeamPick:', poolTeamPick);
+
+      expect(poolTeamPick.actualWins).toBe(13, 'actualWins');
+      expect(poolTeamPick.expectedWins).toBe(10.38, 'expectedWins');
+      expect(poolTeamPick.pickQuality.toFixed(2)).toBe('2.62', 'pickQuality'); // toFixed returns string
+    });
+  });
+});
