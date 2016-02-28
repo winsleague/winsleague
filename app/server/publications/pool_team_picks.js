a9da1718d85f6f1cf40a94660f814f748307d671
Noah Spitzer-Williams <noah@spitzer-williams.com>
2016-02-27 16:56:29 -0800

    new pub

--- app/server/publications/pool_team_picks.js
+++ app/server/publications/pool_team_picks.js
@@ -1,7 +1,13 @@
+Meteor.publish('poolTeamPicks.single', function(poolTeamPickId) {
+  if (!poolTeamPickId) return this.ready();
+  check(poolTeamPickId, String);
+
+  return PoolTeamPicks.find(poolTeamPickId);
+});
+
 Meteor.publish('poolTeamPicks.ofPoolTeam', function(poolTeamId) {
   if (!poolTeamId) return this.ready();
   check(poolTeamId, String);
 
   return PoolTeamPicks.find({ poolTeamId });
 });
-
