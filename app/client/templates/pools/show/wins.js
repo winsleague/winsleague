d6e3d5adaf760e16771cd26db93d85502ac38b3d
Noah Spitzer-Williams <noah@spitzer-williams.com>
2016-02-28 14:37:09 -0800

    much tighter!

--- app/client/templates/pools/show/wins.js
+++ app/client/templates/pools/show/wins.js
@@ -1,40 +1,24 @@
 Template.poolsShowWins.helpers({
-  poolId: () => Template.instance().getPoolId(),
-
-  seasonId: () => Template.instance().getSeasonId(),
-
   poolTeams: () => {
-    const poolId = Template.instance().getPoolId();
+    const poolId = Template.currentData().poolId;
     return PoolTeams.find({ poolId }, { sort: { totalWins: -1, totalPlusMinus: -1 } });
   },
 
   editAllowed: (poolTeam) => {
-    const pool = Pools.findOne(poolTeam.poolId);
     return (Meteor.userId() === poolTeam.userId ||
-    Meteor.userId() === _.get(pool, 'commissionerUserId'));
+      Template.currentData().isCommissioner);
   },
 });
 
 Template.poolsShowWins.onCreated(function() {
-  this.getPoolId = () => FlowRouter.getParam('poolId');
-
-  this.getPool = () => Pools.findOne(this.getPoolId());
-
-  this.getLeagueId = () => _.get(this.getPool(), 'leagueId');
-
-  this.getSeasonId = () => {
-    const seasonId = FlowRouter.getParam('seasonId');
-    if (seasonId) return seasonId;
-    const season = Modules.seasons.getLatestByLeagueId(this.getLeagueId());
-    return _.get(season, '_id');
-  };
+  new SimpleSchema({
+    poolId: { type: String },
+    seasonId: { type: String },
+    isCommissioner: { type: Boolean },
+  }).validate(this.data);
 
   this.autorun(() => {
-    this.subscribe('pools.single', this.getPoolId(), () => {
-      log.debug(`pools.single subscription ready: ${Pools.find(this.getPoolId()).count()}`);
-    });
-
-    this.subscribe('poolTeams.ofPool', this.getPoolId(), this.getSeasonId(), () => {
+    this.subscribe('poolTeams.ofPool', this.data.poolId, this.data.seasonId, () => {
       log.debug(`poolTeams.of_pool subscription ready: ${PoolTeams.find().count()}`);
     });
   });
