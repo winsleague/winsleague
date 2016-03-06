3dd4e565e99e55d00ee020bcfdce9bb57a8e28b9
Noah Spitzer-Williams <noah@spitzer-williams.com>
2016-03-05 19:43:40 -0800

    use emails of the players

--- app/server/modules/weekly_report.js
+++ app/server/modules/weekly_report.js
@@ -19,10 +19,12 @@ Modules.server.weeklyReport = {
     const poolTeams = PoolTeams.find({ poolId, seasonId },
       { sort: { totalWins: -1, totalPlusMinus: -1 } });
 
+    const playerEmails = Modules.server.weeklyReport.getPlayerEmails(poolId, seasonId);
+
     Mailer.send({
-      to: 'noahsw@gmail.com',           // 'To: ' address. Required.
+      to: playerEmails,
       subject: `Wins Leaderboard for ${poolName}`,
-      template: 'weeklyEmail',               // Required.
+      template: 'weeklyEmail',
       data: {
         poolId,
         seasonId,
@@ -59,5 +61,27 @@ Modules.server.weeklyReport = {
       },
     ]);
     return poolAggregation.map(result => result._id);
+  },
+
+  getPlayerEmails(poolId, seasonId) {
+    // each userId can have multiple emails
+    // each email has an address property and a verified property
+    // return in the format 'name <email@domain.com>, name <email@domain.com>'
+
+    const players = PoolTeams.find({ poolId, seasonId })
+      .map(poolTeam => {
+        return {
+          _id: poolTeam.userId,
+          teamName: poolTeam.userTeamName,
+        };
+      });
+
+    const emailArray = _.flatten(players.map(player => {
+      return _.flatten(Meteor.users.find(player._id)
+        .map(user => user.emails))
+        .map(email => `${player.teamName} <${email.address}>`);
+    }));
+
+    return emailArray.join(', ');
   },
 };
