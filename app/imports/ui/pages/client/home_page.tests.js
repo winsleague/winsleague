6e94508f419181a8d5bc31c2d824aa344317c256
Noah Spitzer-Williams <noah@spitzer-williams.com>
2016-07-10 19:42:36 -0700

    got homepage test to pass!

--- app/imports/ui/pages/client/home_page.tests.js
+++ app/imports/ui/pages/client/home_page.tests.js
@@ -1,13 +1,13 @@
+/* eslint-env mocha */
 /* eslint-disable func-names, prefer-arrow-callback */
 
 import { Meteor } from 'meteor/meteor';
-
+import { chai } from 'meteor/practicalmeteor:chai';
 import { Template } from 'meteor/templating';
 import { _ } from 'meteor/underscore';
 import { $ } from 'meteor/jquery';
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { sinon } from 'meteor/practicalmeteor:sinon';
-
 
 import { withRenderedTemplate } from '../../test-helpers.js';
 import '../home_page.js';
@@ -28,10 +28,8 @@ describe('Home_page', function () {
 
   it('renders correctly with simple data', function () {
     withRenderedTemplate('Home_page', {}, el => {
-      const renderedText = $(el).find('.list-items input[type=text]')
-        .map((i, e) => $(e).val())
-        .toArray();
-      chai.assert.deepEqual(renderedText, 'hi');
+      const renderedText = $(el).find('h2').text();
+      chai.assert.equal(renderedText, 'Simple Fantasy Sports');
     });
   });
 });
