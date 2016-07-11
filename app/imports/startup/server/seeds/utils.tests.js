80d3bd9036c9ffb800dc849d0f7d29c3f614c9c3
Noah Spitzer-Williams <noah@spitzer-williams.com>
2016-07-10 19:23:58 -0700

    utils test passes!

---
+++ app/imports/startup/server/seeds/utils.tests.js
@@ -0,0 +1,24 @@
+/* eslint-env mocha */
+/* eslint-disable func-names, prefer-arrow-callback */
+
+import utils from './utils';
+import { Leagues } from '../../../api/leagues/leagues';
+import { LeagueTeams } from '../../../api/league_teams/league_teams';
+
+import { chai } from 'meteor/practicalmeteor:chai';
+
+describe('Utils', function () {
+  describe('initializeLeagues', function () {
+    beforeEach(() => {
+      utils.initializeLeagues();
+    });
+    
+    it('should seed the Leagues', function () {
+      chai.assert.isAbove(Leagues.find().count(), 0);
+    });
+
+    it('should seed the League Teams', function () {
+      chai.assert.isAbove(LeagueTeams.find().count(), 0);
+    });
+  });
+});
