a4e7abebf4be73f664d0aa8fa8c45bcbcf22d9f5
Noah Spitzer-Williams <noah@spitzer-williams.com>
2016-07-14 21:56:00 -0700

    got test to pass by handling case for when expected wins record doesn't exist

--- app/imports/api/pool_team_picks/server/updater.js
+++ app/imports/api/pool_team_picks/server/updater.js
@@ -17,28 +17,32 @@ export default {
     const actualWins = _.get(seasonLeagueTeam, 'wins', 0);
     const gamesPlayed = (seasonLeagueTeam ? seasonLeagueTeam.totalGames() : 0);
 
-    const expectedWinsFullSeason = LeaguePickExpectedWins.findOne({
+    const expectedWinsRecord = LeaguePickExpectedWins.findOne({
       leagueId: poolTeamPick.leagueId,
       rank: poolTeamPick.pickNumber,
-    }).wins;
-
-    const seasonGameCount = Leagues.findOne(poolTeamPick.leagueId).seasonGameCount;
-
-    const expectedWins = gamesPlayed / seasonGameCount * expectedWinsFullSeason;
-
-    const pickQuality = actualWins - expectedWins;
-
-    log.info('Updating PoolTeamPick:', poolTeamPick,
-      'actualWins:', actualWins,
-      'expectedWins:', expectedWins,
-      'pickQuality:', pickQuality);
-
-    PoolTeamPicks.direct.update(poolTeamPick._id, {
-      $set: {
-        actualWins,
-        expectedWins,
-        pickQuality,
-      },
     });
+
+    if (expectedWinsRecord) {
+      const expectedWinsFullSeason = expectedWinsRecord.wins;
+
+      const seasonGameCount = Leagues.findOne(poolTeamPick.leagueId).seasonGameCount;
+
+      const expectedWins = gamesPlayed / seasonGameCount * expectedWinsFullSeason;
+
+      const pickQuality = actualWins - expectedWins;
+
+      log.info('Updating PoolTeamPick:', poolTeamPick,
+        'actualWins:', actualWins,
+        'expectedWins:', expectedWins,
+        'pickQuality:', pickQuality);
+
+      PoolTeamPicks.direct.update(poolTeamPick._id, {
+        $set: {
+          actualWins,
+          expectedWins,
+          pickQuality,
+        },
+      });
+    }
   },
 };
