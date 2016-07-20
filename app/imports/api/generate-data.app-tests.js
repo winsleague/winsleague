874a09e3adbb6e045caf939ec9915b1509818f59
Noah Spitzer-Williams <noah@spitzer-williams.com>
2016-07-19 20:43:54 -0700

    fixed ESlint warnings

--- app/imports/api/generate-data.app-tests.js
+++ app/imports/api/generate-data.app-tests.js
@@ -18,14 +18,15 @@ Meteor.methods({
   },
 });
 
+let generateData;
 if (Meteor.isClient) {
   // Create a second connection to the server to use to call test data methods
   // We do this so there's no contention w/ the currently tested user's connection
   const testConnection = Meteor.connect(Meteor.absoluteUrl());
 
-  const generateData = denodeify((cb) => {
+  generateData = denodeify((cb) => {
     testConnection.call('generateFixtures', cb);
   });
-
-  export { generateData };
 }
+
+export { generateData };
