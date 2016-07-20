5dd6d881c671e6c7827fd4a79652dc46c2be547f
Noah Spitzer-Williams <noah@spitzer-williams.com>
2016-07-19 21:43:02 -0700

    finished pool hooks test

---
+++ app/imports/api/pools/server/hooks.tests.js
@@ -0,0 +1,26 @@
+/* eslint-env mocha */
+/* eslint-disable func-names, prefer-arrow-callback */
+
+import { Factory } from 'meteor/dburles:factory';
+import log from '../../../utils/log';
+
+import { PoolTeams } from '../../pool_teams/pool_teams';
+import { Pools } from '../pools';
+import './hooks';
+
+import { assert } from 'meteor/practicalmeteor:chai';
+
+describe('Pools Hooks', function () {
+  it('should remove its PoolTeams when Pool is removed', function () {
+    const poolTeam = Factory.create('poolTeam');
+    const poolId = poolTeam.poolId;
+
+    let poolTeamsCount = PoolTeams.find({ poolId }).count();
+    assert.equal(poolTeamsCount, 1);
+
+    Pools.remove(poolId);
+
+    poolTeamsCount = PoolTeams.find({ poolId }).count();
+    assert.equal(poolTeamsCount, 0);
+  });
+});
