4a9657e04fcf6a68eb014f8d5405a4e862135f50
Noah Spitzer-Williams <noah@spitzer-williams.com>
2016-07-19 20:17:59 -0700

    fix Promise.nodeify error by pulling from latest meteor-todos

--- app/imports/api/generate-data.app-tests.js
+++ app/imports/api/generate-data.app-tests.js
@@ -4,7 +4,7 @@ import { Meteor } from 'meteor/meteor';
 import { Factory } from 'meteor/dburles:factory';
 import log from '../startup/log';
 import { resetDatabase } from 'meteor/xolvio:cleaner';
-import { Promise } from 'meteor/promise';
+import { denodeify } from '../utils/denodeify';
 
 Meteor.methods({
   generateFixtures() {
@@ -18,15 +18,14 @@ Meteor.methods({
   },
 });
 
-let generateData;
 if (Meteor.isClient) {
   // Create a second connection to the server to use to call test data methods
   // We do this so there's no contention w/ the currently tested user's connection
   const testConnection = Meteor.connect(Meteor.absoluteUrl());
 
-  generateData = Promise.denodeify((cb) => {
+  const generateData = denodeify((cb) => {
     testConnection.call('generateFixtures', cb);
   });
-}
 
-export { generateData };
+  export { generateData };
+}
