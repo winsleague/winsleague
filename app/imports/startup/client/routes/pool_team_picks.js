1270d0e6b49688f8e9883fda6cd7a76c045ac001
Noah Spitzer-Williams <noah@spitzer-williams.com>
2016-07-28 22:44:01 -0700

    created pool team pick new form

--- app/imports/startup/client/routes/pool_team_picks.js
+++ app/imports/startup/client/routes/pool_team_picks.js
@@ -3,26 +3,28 @@ import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 import { AccountsTemplates } from 'meteor/useraccounts:core';
 
+import '../../../ui/pages/pool-team-picks-new-page';
+
 const group = FlowRouter.group({
   prefix: '/pools/:poolId/teams/:poolTeamId/picks',
 });
 
 // http://app.com/pools/:poolId/teams/:poolTeamId/picks/new
 group.route('/new', {
-  name: 'poolTeamPicksNew',
+  name: 'PoolTeamPicks.new',
   triggersEnter: [AccountsTemplates.ensureSignedIn],
   action(params) {
     log.debug(`We're creating picks for a pool team: ${params.poolTeamId}`);
-    BlazeLayout.render('App_body', { content: 'poolTeamPicksNew' });
+    BlazeLayout.render('App_body', { content: 'PoolTeamPicks_new_page' });
   },
 });
 
 // http://app.com/pools/:poolId/teams/:poolTeamId/picks/:poolTeamPickId/edit
 group.route('/:poolTeamPickId/edit', {
-  name: 'poolTeamPicksEdit',
+  name: 'PoolTeamPicks.edit',
   triggersEnter: [AccountsTemplates.ensureSignedIn],
   action(params) {
     log.debug(`We're editing a pool team pick: ${params.poolTeamId} and ${params.poolTeamPickId}`);
-    BlazeLayout.render('App_body', { content: 'poolTeamPicksEdit' });
+    BlazeLayout.render('App_body', { content: 'PoolTeamPicks_edit_page' });
   },
 });
