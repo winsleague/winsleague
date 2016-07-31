ed2b872fe4b97bdd10c12428e7be920bc9da00f9
Noah Spitzer-Williams <noah@spitzer-williams.com>
2016-07-31 10:12:51 -0700

    randomize username to avoid clashes

--- app/imports/api/users/users.js
+++ app/imports/api/users/users.js
@@ -1,9 +1,10 @@
 import { Factory } from 'meteor/dburles:factory';
+import { Random } from 'meteor/random';
 import faker from 'faker';
 import log from '../../utils/log';
 
 Factory.define('user', Meteor.users, {
-  username: () => faker.lorem.word(),
+  username: () => `${faker.lorem.word()}-${Random.id()}`,
   profile: {
     firstName: () => faker.name.firstName(),
     lastName: () => faker.name.lastName(),
