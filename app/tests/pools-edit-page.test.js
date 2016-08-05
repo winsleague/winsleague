66d1e97b46c17ad0d56e3843cd70d88db8e719c2
Noah Spitzer-Williams <noah@spitzer-williams.com>
2016-08-05 15:46:55 -0700

    update to pools.edit test

--- app/tests/pools-edit-page.test.js
+++ app/tests/pools-edit-page.test.js
@@ -2,58 +2,42 @@
 // These are Chimp globals */
 /* globals browser server assert */
 
-describe('pool edit page ui @watch', () => {
+describe('Pools.edit page ui', () => {
   beforeEach(() => {
-    browser.url('http://localhost:3100');
-
-    console.log('1');
     server.call('logout');
 
-    console.log('0');
     browser
       .timeoutsAsyncScript(5000)
       .executeAsync(function (done) {
-      Meteor.logout(done);
-    });
-    console.log('2');
+        Meteor.logout(done);
+      });
+
     server.call('generateFixtures');
 
-    console.log('3');
     browser.executeAsync(function (done) {
       Meteor.loginWithPassword('test@test.com', 'test', done);
     });
 
-    /*
-    // login
-    console.log('0');
-    browser.waitForExist('button#at-nav-button');
-    console.log('1');
-    browser.click('button#at-nav-button');
-    console.log('2');
-    browser.waitForExist('input#at-field-email');
-    console.log('2b');
-    browser.setValue('input#at-field-email', 'test@test.com');
-    console.log('3');
-    browser.setValue('input#at-field-password', 'test');
-    console.log('4');
-    browser.click('button#at-btn');
-     */
-    console.log('5');
     browser.url('http://localhost:3100');
-    browser.waitForExist('h3.Pools_show');
-    console.log('6');
 
+    browser.waitForVisible('h3.Pools_show');
 
-    // browse to pool
+    browser.click('h3.Pools_show>a');
+
+    browser.waitForVisible('a#Pools_edit');
+    browser.click('a#Pools_edit');
+
+    browser.waitForVisible('input[name="name"]');
   });
 
-  it('can create a list @watch', () => {
+  it('can edit a pool @watch', () => {
     const name = 'Dumber';
 
-    assert.equal(1, 1);
+    browser.setValue('input[name="name"]', name);
+    browser.submitForm('form');
 
-    // client.click('.js-new-list');
+    browser.waitForVisible('h3.Pools_show');
 
-    // assert.equal(countLists(), initialCount + 1);
+    assert.equal(browser.getText('#Pools_title'), name);
   });
 });
