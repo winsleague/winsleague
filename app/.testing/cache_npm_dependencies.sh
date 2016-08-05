f3999ba74f73f19c96659c7ff2b6e3f38cf48369
Noah Spitzer-Williams <noah@spitzer-williams.com>
2016-08-05 12:01:41 -0700

    added logging and disabled global spacejam

--- app/.testing/cache_npm_dependencies.sh
+++ app/.testing/cache_npm_dependencies.sh
@@ -1,4 +1,10 @@
 # Cache npm deps
+echo "Installing Chimp …"
 if [ ! -e /home/ubuntu/nvm/versions/node/v5.2.0/lib/node_modules/chimp/bin/chimp.js ]; then npm install -g chimp; fi
-if [ ! -e /home/ubuntu/nvm/versions/node/v5.2.0/lib/node_modules/spacejam/bin/spacejam ]; then npm install -g spacejam; fi
+
+# Disabled while we use the forked Spacejam in package.json
+# echo "Installing Spacejam …"
+# if [ ! -e /home/ubuntu/nvm/versions/node/v5.2.0/lib/node_modules/spacejam/bin/spacejam ]; then npm install -g spacejam; fi
+
+echo "Installing local packages …"
 npm install
