70f44235922a3516324fa5a2c0eafa03d2ea9cb2
Noah Spitzer-Williams <noah@spitzer-williams.com>
2016-08-06 14:26:10 -0700

    pools.new test

---
+++ app/tests/pools-new-page.test.js
@@ -0,0 +1,43 @@
+/* eslint-env mocha */
+// These are Chimp globals */
+/* globals browser server assert */
+
+describe('Pools.new page ui', () => {
+  beforeEach(() => {
+    // we must navigate to client first so Meteor methods are available
+    browser.url('http://localhost:3100');
+
+    browser.timeouts('script', 5000);
+
+    server.call('logout');
+
+    browser.executeAsync(function (done) {
+      Meteor.logout(done);
+    });
+
+    server.call('generateFixtures');
+
+    browser.executeAsync(function (done) {
+      Meteor.loginWithPassword('test@test.com', 'test', done);
+    });
+
+    browser.url('http://localhost:3100');
+
+    // go to the Pools.show page
+    browser.waitForVisible('a#Pools_new_link');
+    browser.click('a#Pools_new_link');
+  });
+
+  it('can create a pool', () => {
+    const newTitle = 'pool name';
+
+    browser.waitForVisible('input#name');
+    browser.setValue('input#name', newTitle);
+
+    browser.submitForm('form');
+
+    browser.waitForExist('h3.Pools_show');
+
+    assert.equal(browser.getText('#Pools_title'), `2015 ${newTitle}`);
+  });
+});
