d3f586e7734ec78f76133d5bc82d8c463f6ef6ed
Noah Spitzer-Williams <noah@spitzer-williams.com>
2016-08-06 15:02:54 -0700

    pool teams new test

---
+++ app/tests/pool-teams-new-page.test.js
@@ -0,0 +1,54 @@
+/* eslint-env mocha */
+// These are Chimp globals */
+/* globals browser server assert */
+
+const setup = () => {
+  // we must navigate to client first so Meteor methods are available
+  browser.url('http://localhost:3100');
+
+  browser.timeouts('script', 5000);
+
+  server.call('logout');
+
+  browser.executeAsync(function (done) {
+    Meteor.logout(done);
+  });
+
+  server.call('generateFixtures');
+
+  browser.executeAsync(function (done) {
+    Meteor.loginWithPassword('test@test.com', 'test', done);
+  });
+
+  browser.url('http://localhost:3100');
+};
+
+describe('PoolTeams.new page ui', () => {
+  beforeEach(() => {
+    setup();
+
+    // go to the Pools.show page
+    browser.waitForVisible('h3.Pools_show');
+    browser.click('h3.Pools_show>a');
+
+    browser.waitForVisible('a#PoolTeams_new');
+    browser.click('a#PoolTeams_new');
+  });
+
+  it('can create a pool team', () => {
+    const newEmail = 'new@test.com';
+    const newTeamName = 'pool team';
+
+    browser.waitForVisible('input#userEmail');
+    browser.setValue('input#userEmail', newEmail);
+
+    browser.waitForVisible('input#userTeamName');
+    browser.setValue('input#userTeamName', newTeamName);
+
+    browser.submitForm('form');
+
+    browser.waitForExist('h3#PoolTeams_show_title');
+
+    assert.equal(browser.getText('h3#PoolTeams_show_title'), newTeamName);
+  });
+});
