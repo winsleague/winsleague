4a2e54b809811658c9e65d9e9fa9891392d13053
Noah Spitzer-Williams <noah@spitzer-williams.com>
2016-08-06 11:09:59 -0700

    added checks

--- app/imports/api/pool_team_picks/server/publications.js
+++ app/imports/api/pool_team_picks/server/publications.js
@@ -1,26 +1,27 @@
 /* eslint-disable prefer-arrow-callback */
 
 import { Meteor } from 'meteor/meteor';
+import { check, Match } from 'meteor/check';
 
 import { PoolTeamPicks } from '../pool_team_picks';
 
 Meteor.publish('poolTeamPicks.single', function poolTeamPicksSingle(poolTeamPickId) {
+  check(poolTeamPickId, Match.Maybe(String));
   if (!poolTeamPickId) return this.ready();
-  check(poolTeamPickId, String);
 
   return PoolTeamPicks.find(poolTeamPickId);
 });
 
 Meteor.publish('poolTeamPicks.ofPoolTeam', function poolTeamPicksOfPoolTeam(poolTeamId) {
+  check(poolTeamId, Match.Maybe(String));
   if (!poolTeamId) return this.ready();
-  check(poolTeamId, String);
 
   return PoolTeamPicks.find({ poolTeamId });
 });
 
 Meteor.publish('poolTeamPicks.ofPool', function poolTeamPicksOfPool(poolId, seasonId = null) {
+  check(poolId, Match.Maybe(String));
   if (!poolId) return this.ready();
-  check(poolId, String);
 
   if (seasonId) {
     check(seasonId, String);
