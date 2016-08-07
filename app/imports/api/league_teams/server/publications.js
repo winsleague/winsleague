96d9636bef14629e7604a3292847c4c76f22d032
Noah Spitzer-Williams <noah@spitzer-williams.com>
2016-08-06 17:29:52 -0700

    smarter check

--- app/imports/api/league_teams/server/publications.js
+++ app/imports/api/league_teams/server/publications.js
@@ -1,8 +1,9 @@
 import { Meteor } from 'meteor/meteor';
+import { check, Match } from 'meteor/check';
 
 import { LeagueTeams } from '../league_teams';
 
 Meteor.publish('leagueTeams.ofLeague', leagueId => {
-  check(leagueId, String);
+  check(leagueId, Match.Maybe(String));
   return LeagueTeams.find({ leagueId });
 });
