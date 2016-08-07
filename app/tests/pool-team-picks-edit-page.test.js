426258f008460141c81d3c427a872cfe0966dd92
Noah Spitzer-Williams <noah@spitzer-williams.com>
2016-08-06 21:57:13 -0700

    pool team pick edit test

---
+++ app/tests/pool-team-picks-edit-page.test.js
@@ -0,0 +1,57 @@
+/* eslint-env mocha */
+// These are Chimp globals */
+/* globals browser server assert */
+
+const setup = () => {
+  // we must navigate to client first so Meteor methods are available
+  browser.url('http://localhost:3100');
+
+  browser.timeouts('script', 5000);
+
+  server.call('logout');
+
+  browser.executeAsync(function (done) {
+    Meteor.logout(done);
+  });
+
+  server.call('generateFixtures');
+
+  browser.executeAsync(function (done) {
+    Meteor.loginWithPassword('test@test.com', 'test', done);
+  });
+
+  browser.url('http://localhost:3100');
+};
+
+describe('PoolTeamPicks.edit page ui', () => {
+  beforeEach(() => {
+    setup();
+
+    // go to the Pools.show page
+    browser.waitForVisible('h3.Pools_show');
+    browser.click('h3.Pools_show>a');
+
+    // go to PoolTeams.show page
+    browser.waitForVisible('a.PoolTeams_show:nth-Child(1)');
+    browser.click('a.PoolTeams_show:nth-Child(1)');
+
+    // go to PoolTeamPicks.edit page
+    browser.waitForVisible('a.PoolTeamPicks_edit:nth-Child(1)');
+    browser.click('a.PoolTeamPicks_edit:nth-Child(1)');
+  });
+
+  it('can edit a pool team pick', () => {
+    browser.waitForVisible('select#leagueTeamId');
+    browser.selectByIndex('select#leagueTeamId', 4);
+
+    const pickNumber = 6;
+    browser.waitForVisible('select#pickNumber');
+    browser.selectByIndex('select#pickNumber', pickNumber);
+
+    browser.submitForm('form');
+
+    browser.waitForExist('h3#PoolTeams_show_title');
+
+    assert.equal(browser.getText('td.PoolTeamPick:nth-Child(1)'), pickNumber);
+  });
+});
