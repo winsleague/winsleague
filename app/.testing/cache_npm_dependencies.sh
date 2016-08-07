684ea69f172add9185dfa2b01902f314f29cb8ab
Noah Spitzer-Williams <noah@spitzer-williams.com>
2016-08-07 11:24:29 -0700

    fix the chimp version so CI is predictable

--- app/.testing/cache_npm_dependencies.sh
+++ app/.testing/cache_npm_dependencies.sh
@@ -3,7 +3,7 @@ echo "Pruning …"
 meteor npm prune
 
 echo "Installing Chimp …"
-if [ ! -e /home/ubuntu/nvm/versions/node/v5.2.0/lib/node_modules/chimp/bin/chimp.js ]; then npm install -g chimp; fi
+if [ ! -e /home/ubuntu/nvm/versions/node/v5.2.0/lib/node_modules/chimp/bin/chimp.js ]; then npm install -g chimp@0.39.3; fi
 
 # Disabled while we use the forked Spacejam in package.json (needed to instrument coverage)
 # echo "Installing Spacejam …"
