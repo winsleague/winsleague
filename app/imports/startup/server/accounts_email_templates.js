3eec0022becc1cfc0cb4a2ce169811aa354c353b
Noah Spitzer-Williams <noah@spitzer-williams.com>
2016-08-09 22:10:58 -0700

    fixed email template

--- app/imports/startup/server/accounts_email_templates.js
+++ app/imports/startup/server/accounts_email_templates.js
@@ -4,10 +4,11 @@ Accounts.emailTemplates.siteName = 'Wins League';
 Accounts.emailTemplates.from = 'Wins League <team@winsleague.com>';
 
 Accounts.emailTemplates.enrollAccount.subject = (user) =>
-  `Welcome to Wins League, ${user.profile.name}`;
+  'Welcome to Wins League';
 
 Accounts.emailTemplates.enrollAccount.text = (user, url) =>
-  `You have registered for Wins League!
+  `Wins League is a simple form fantasy sports, but just as much fun.
+  
 To activate your account, simply click the link below:
  
 ${url}`;
