c5b819f67e5d6b2fc16860c8dd680d879b26b8c3
Noah Spitzer-Williams <noah@spitzer-williams.com>
2016-08-17 21:09:41 -0700

    capturing exit codes of both tests so we can run them both in case the first fails. but we'll also fail the whole thing if one of them fails

--- app/.testing/unit_and_full_app_run.sh
+++ app/.testing/unit_and_full_app_run.sh
@@ -1,5 +1,16 @@
+#!/bin/bash
+
 mkdir .coverage
 
 meteor npm run test
+UNIT_RESULT=$?
 
 meteor npm run test-app
+FULL_APP_RESULT=$?
+
+if [[ "$UNIT_RESULT" -eq 0 && "$FULL_APP_RESULT" -eq 0 ]]
+then
+  echo "Both tests pass!"
+else
+  echo "One of the tests failed!" >&2
+fi
