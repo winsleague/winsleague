911d612f0c7d7d75688ed2ae7ee8235a88982b3d
Noah Spitzer-Williams <noah@spitzer-williams.com>
2016-08-18 22:03:06 -0700

    formatting and refactoring

--- app/imports/api/seasons/finder.js
+++ app/imports/api/seasons/finder.js
@@ -3,6 +3,10 @@ import LeagueFinder from '../leagues/finder';
 
 export default {
   getByYear(league, year = (new Date()).getFullYear()) {
+    if (!league) {
+      throw new Error('League is not found!');
+    }
+
     let leagueId;
     if (typeof league === 'string') {
       leagueId = LeagueFinder.getIdByName(league);
@@ -14,19 +18,35 @@ export default {
   },
 
   getLatestByLeague(league) {
+    if (!league) {
+      throw new Error('League is not found!');
+    }
+
     return this.getLatestByLeagueId(league._id);
   },
 
   getLatestByLeagueId(leagueId) {
+    if (!leagueId) {
+      throw new Error('League is not found!');
+    }
+
     return Seasons.findOne({ leagueId }, { sort: { year: -1 } });
   },
 
   getLatestByLeagueName(leagueName) {
+    if (!leagueName) {
+      throw new Error('League is not found!');
+    }
+
     const leagueId = LeagueFinder.getIdByName(leagueName);
     return this.getLatestByLeagueId(leagueId);
   },
 
   getLatestCursorByLeagueId(leagueId) {
+    if (!leagueId) {
+      throw new Error('League is not found!');
+    }
+
     return Seasons.find({ leagueId }, { sort: { year: -1 }, limit: 1 });
   },
 };
