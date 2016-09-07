5ee394fd7ae7fff253f5e67ca4bf02154520aa3d
Noah Spitzer-Williams <noah@spitzer-williams.com>
2016-09-06 22:06:16 -0700

    fixed redirect delay problem

--- app/imports/startup/client/routes/main.js
+++ app/imports/startup/client/routes/main.js
@@ -3,6 +3,7 @@ import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 
 import '../../../ui/pages/home_page';
+import '../../../ui/pages/user-dashboard';
 import '../../../ui/pages/app-not-found';
 
 log.info('Setting up main routes');
@@ -10,7 +11,13 @@ log.info('Setting up main routes');
 FlowRouter.route('/', {
   name: 'App.home',
   action() {
-    BlazeLayout.render('App_body', { content: 'Home_page' });
+    Tracker.autorun(function() {
+      if (!Meteor.userId()) {
+        BlazeLayout.render('App_body', { content: 'Home_page' });
+      } else {
+        BlazeLayout.render('App_body', { content: 'User_dashboard' });
+      }
+    });
   },
 });
 
