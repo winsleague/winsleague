eabe55f56e8d895bf60229f167f494ed032d4645
Noah Spitzer-Williams <noah@spitzer-williams.com>
2016-09-06 21:36:07 -0700

    changed docker image for mup based on https://github.com/kadirahq/meteor-up/issues/172

---
+++ .deploy/staging/mup.js
@@ -0,0 +1,41 @@
+module.exports = {
+  servers: {
+    one: {
+      host: "192.168.33.17",
+      username: "vagrant",
+      password: "vagrant",
+    },
+  },
+
+  meteor: {
+    name: "winsleague",
+    path: "../../app/",
+    servers: {
+      one: {},
+    },
+    buildOptions: {
+      debug: true,
+    },
+    env: {
+      ROOT_URL: "http://www.winsleague.com",
+      MONGO_URL: "mongodb://localhost/meteor",
+      LOGGLY_TOKEN: "",
+      ROLLBAR_SERVER_ACCESS_TOKEN: "",
+      ROLLBAR_CLIENT_ACCESS_TOKEN: "",
+      ROLLBAR_ENVIRONMENT: "staging",
+      KADIRA_APP_ID: "",
+      KADIRA_APP_SECRET: "",
+      MAIL_URL: ""
+    },
+    dockerImage: "abernix/meteord:base",
+    deployCheckWaitTime: 60,
+  },
+
+  mongo: {
+    oplog: true,
+    port: 27017,
+    servers: {
+      one: {},
+    },
+  },
+};
