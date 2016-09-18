876c07281a523187b25e0afcc64a070fb1d85234
Noah Spitzer-Williams <noah@spitzer-williams.com>
2016-09-18 10:28:53 -0700

    styling

--- app/imports/api/games/games.js
+++ app/imports/api/games/games.js
@@ -101,18 +101,18 @@ Games.schema = new SimpleSchema({
 Games.attachSchema(Games.schema);
 
 function ordinalSuffixOf(i) {
-  const j = i % 10,
-    k = i % 100;
+  const j = i % 10;
+  const k = i % 100;
   if (j === 1 && k !== 11) {
-    return i + "st";
+    return `${i}st`;
   }
   if (j === 2 && k !== 12) {
-    return i + "nd";
+    return `${i}nd`;
   }
   if (j === 3 && k !== 13) {
-    return i + "rd";
+    return `${i}rd`;
   }
-  return i + "th";
+  return `${i}th`;
 }
 
 Games.helpers({
