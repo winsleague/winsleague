e2230554e7c9d08d9a98fa68587d2dc8ab270d7c
Noah Spitzer-Williams <noah@spitzer-williams.com>
2016-09-26 21:54:08 -0700

    update game winners and losers

---
+++ app/imports/api/games/server/updater.js
@@ -0,0 +1,34 @@
+import log from '../../../utils/log';
+
+import { Games } from '../../games/games';
+import '../../season_league_teams/server/hooks';
+
+export default {
+  updateWinner(game) {
+    if (game.status !== 'completed') {
+      return;
+    }
+
+    let winnerTeamId;
+    let loserTeamId;
+    if (game.homeScore > game.awayScore) {
+      winnerTeamId = game.homeTeamId;
+      loserTeamId = game.awayTeamId;
+    } else if (game.awayScore > game.homeScore) {
+      winnerTeamId = game.awayTeamId;
+      loserTeamId = game.homeTeamId;
+    }
+
+    if (winnerTeamId) {
+      log.info(`Update game ${game._id} winner to ${winnerTeamId} and loser to ${loserTeamId}`);
+
+      Games.direct.update(game._id,
+        {
+          $set: {
+            winnerTeamId,
+            loserTeamId,
+          },
+        });
+    }
+  },
+};
