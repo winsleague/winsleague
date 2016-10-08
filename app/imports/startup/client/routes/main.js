5833ffa368a8b11d54c5e3d25b95f41cf481e136
Noah Spitzer-Williams <noah@spitzer-williams.com>
2016-10-08 11:43:44 -0700

    got the redirect working

--- app/imports/startup/client/routes/main.js
+++ app/imports/startup/client/routes/main.js
@@ -1,5 +1,6 @@
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
+import store from 'store';
 import log from '../../../utils/log';
 
 import '../../../ui/pages/home_page';
@@ -8,8 +9,30 @@ import '../../../ui/pages/app-not-found';
 
 log.info('Setting up main routes');
 
+function storeLastUrl(context) {
+  // need to allow users to go home when they want to, but redirect when they don't
+  if (context.path !== '/') {
+    store.set('lastUrl', context.path);
+  }
+}
+
+function redirectToLastUrl(context) {
+  if (context.queryParams.force) {
+    // don't redirect
+    return;
+  }
+
+  const url = store.get('lastUrl');
+  if (url && url !== '/?force=true') {
+    FlowRouter.redirect(url);
+  }
+}
+
+FlowRouter.triggers.enter([storeLastUrl]);
+
 FlowRouter.route('/', {
   name: 'App.home',
+  triggersEnter: [redirectToLastUrl],
   action() {
     Tracker.autorun(() => {
       if (!Meteor.userId()) {
@@ -26,3 +49,4 @@ FlowRouter.notFound = {
     BlazeLayout.render('App_body', { content: 'App_notFound' });
   },
 };
+
