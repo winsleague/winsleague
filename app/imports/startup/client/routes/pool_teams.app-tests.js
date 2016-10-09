c77307b6cd505b75e192adea960f76b045ccdcdb
Noah Spitzer-Williams <noah@spitzer-williams.com>
2016-10-09 15:53:57 -0700

    styling

--- app/imports/startup/client/routes/pool_teams.app-tests.js
+++ app/imports/startup/client/routes/pool_teams.app-tests.js
@@ -30,8 +30,8 @@ if (Meteor.isClient) {
         .then(waitForSubscriptions)
     );
 
-    afterEach(function (done) {
-      Meteor.logout(function () {
+    afterEach((done) => {
+      Meteor.logout(() => {
         log.info('Logged out');
         FlowRouter.go('/?force=true');
         FlowRouter.watchPathChange();
