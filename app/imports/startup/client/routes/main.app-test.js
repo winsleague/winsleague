bc84b254f029175d554e1b649591d93f0731fbea
Noah Spitzer-Williams <noah@spitzer-williams.com>
2016-10-09 15:36:49 -0700

    more stable homepage tests

--- app/imports/startup/client/routes/main.app-test.js
+++ app/imports/startup/client/routes/main.app-test.js
@@ -1,41 +1,34 @@
 /* eslint-env mocha */
 
 import { Meteor } from 'meteor/meteor';
-import { Tracker } from 'meteor/tracker';
-import { DDP } from 'meteor/ddp-client';
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { assert } from 'chai';
-import { Promise } from 'meteor/promise';
-import { denodeify } from '../../../utils/denodeify';
 import { $ } from 'meteor/jquery';
+import log from '../../../utils/log';
 
-import { generateData } from './../../../api/generate-data.app-tests.js';
+import { waitForSubscriptions, afterFlushPromise, resetRoute, login } from './helpers.app-tests';
+import { generateData } from './../../../api/generate-data.app-tests';
 
-
-// Utility -- returns a promise which resolves when all subscriptions are done
-const waitForSubscriptions = () => new Promise(resolve => {
-  const poll = Meteor.setInterval(() => {
-    if (DDP._allSubscriptionsReady()) {
-      Meteor.clearInterval(poll);
-      resolve();
-    }
-  }, 200);
-});
-
-// Tracker.afterFlush runs code when all consequent of a tracker based change
-//   (such as a route change) have occured. This makes it a promise.
-const afterFlushPromise = denodeify(Tracker.afterFlush);
 
 if (Meteor.isClient) {
   describe('Full-app test of Homepage', function () {
-    this.timeout(5000);
+    this.timeout(10000);
 
-    // First, ensure the data that we expect is loaded on the server
-    //   Then, route the app to the homepage
     beforeEach(() =>
-      generateData()
-        .then(() => FlowRouter.go('/'))
-        .then(waitForSubscriptions));
+      resetRoute()
+        .then(() => generateData())
+        .then(login)
+        .then(waitForSubscriptions)
+    );
+
+    afterEach(function (done) {
+      Meteor.logout(function () {
+        log.info('Logged out');
+        FlowRouter.go('/?force=true');
+        FlowRouter.watchPathChange();
+        done();
+      });
+    });
 
     it('has title on homepage', () => {
       return afterFlushPromise()
