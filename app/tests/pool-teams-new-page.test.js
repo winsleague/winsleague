df549233a0dae31f3f0c8c1245e363e5dd1893a6
Noah Spitzer-Williams <noah@spitzer-williams.com>
2016-10-10 08:33:10 -0700

    put implicit timeouts back in because they caused CI to fail

--- app/tests/pool-teams-new-page.test.js
+++ app/tests/pool-teams-new-page.test.js
@@ -6,6 +6,8 @@ const setup = () => {
   // we must navigate to client first so Meteor methods are available
   browser.url('http://localhost:3100');
 
+  browser.timeouts('implicit', 5000);
+
   server.call('logout');
 
   browser.executeAsync(function (done) {
