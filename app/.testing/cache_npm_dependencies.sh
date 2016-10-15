64a7c5934754949882ead9d682e144ae17ad0c3b
Noah S-W <noahsw@users.noreply.github.com>
2016-10-15 16:31:24 -0700

    Feature/332 loggly dependency + Yarn support! (#345)
    * switched to node 4.5 because that's what meteor 1.4 uses
    
    * switch to yarn
    
    * remove node_shrinkwrap because yarn doesn't need it
    
    * upgrade loggly for security reason
    
    * cache yarn stuff and don't install yarn if it's already there

--- app/.testing/cache_npm_dependencies.sh
+++ app/.testing/cache_npm_dependencies.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-# Cache npm deps
-echo "Pruning …"
-meteor npm prune
-
 # Disabled while we use the forked Spacejam in package.json (needed to instrument coverage)
 # echo "Installing Spacejam …"
-# if [ ! -e /home/ubuntu/nvm/versions/node/v5.2.0/lib/node_modules/spacejam/bin/spacejam ]; then npm install -g spacejam; fi
+# if [ ! -e /home/ubuntu/nvm/versions/node/v4.5.0/lib/node_modules/spacejam/bin/spacejam ]; then npm install -g spacejam; fi
 
 echo "Installing local packages …"
-meteor npm install
+yarn
--- app/.testing/install_chimp.sh
+++ app/.testing/install_chimp.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Installing Chimp …"
-if [ ! -e /home/ubuntu/nvm/versions/node/v5.2.0/lib/node_modules/chimp/bin/chimp.js ]; then npm install -g chimp@0.40.7; fi
+if [ ! -e /home/ubuntu/nvm/versions/node/v4.5.0/lib/node_modules/chimp/bin/chimp.js ]; then npm install -g chimp@0.40.7; fi
