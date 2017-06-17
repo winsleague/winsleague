11c2ed4b3c7858f7f45bd40f4e617acd44e29d2c
Noah Spitzer-Williams <noah@spitzer-williams.com>
2017-06-16 23:15:03 -0600

    align node version with meteor node version

--- app/.testing/cache_npm_dependencies.sh
+++ app/.testing/cache_npm_dependencies.sh
@@ -2,7 +2,7 @@
 
 # Disabled until we get code coverage working. Right now it's a major pain.
 # echo "Installing Spacejam …"
-# if [ ! -e /home/ubuntu/nvm/versions/node/v4.6.2/lib/node_modules/spacejam/bin/spacejam ]; then yarn global add spacejam; fi
+# if [ ! -e /home/ubuntu/nvm/versions/node/v4.8.1/lib/node_modules/spacejam/bin/spacejam ]; then yarn global add spacejam; fi
 
 echo "Installing local packages …"
 yarn --verbose
