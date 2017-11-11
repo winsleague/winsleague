92700ae1f5cccb70a365d31b4d5ad94f6214c434
Noah Spitzer-Williams <noah@spitzer-williams.com>
2017-11-10 21:26:30 -0800

    initial upgrade to meteor 1.6 and node 8.8.1

--- app/.testing/cache_npm_dependencies.sh
+++ app/.testing/cache_npm_dependencies.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-# Disabled until we get code coverage working. Right now it's a major pain.
-# echo "Installing Spacejam …"
-# if [ ! -e /home/ubuntu/nvm/versions/node/v4.8.1/lib/node_modules/spacejam/bin/spacejam ]; then yarn global add spacejam; fi
-
 echo "Installing local packages …"
 yarn --verbose
--- app/.testing/install_chimp.sh
+++ app/.testing/install_chimp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Installing Chimp …"
-if [ ! -e /home/ubuntu/nvm/versions/node/v4.8.1/lib/node_modules/chimp/bin/chimp.js ]; then yarn global add chimp@0.49.0; fi
+if [ ! -e /home/ubuntu/nvm/versions/node/v8.8.1/lib/node_modules/chimp/bin/chimp.js ]; then yarn global add chimp@0.49.0; fi
 chimp --versions
