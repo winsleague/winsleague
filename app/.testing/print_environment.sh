78d256440578be67ab4cb6863e04bca3677d346a
Noah Spitzer-Williams <noah@spitzer-williams.com>
2017-11-20 09:30:20 -0800

    added which node

--- app/.testing/print_environment.sh
+++ app/.testing/print_environment.sh
@@ -4,6 +4,7 @@ echo "environment versions:"
 . /etc/os-release
 echo "operating system $VERSION"
 echo "kernel $(uname -a)"
+echo "which node: $(which node)"
 echo "node $(node -v)"
 echo "$(meteor --version)"
 echo "meteor node $(meteor node -v)"
