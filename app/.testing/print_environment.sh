2d34b760d638e146b6f8727b2d29da6900c190e6
Noah Spitzer-Williams <noah@spitzer-williams.com>
2017-11-19 19:50:01 -0800

    print OS version

--- app/.testing/print_environment.sh
+++ app/.testing/print_environment.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 echo "environment versions:"
-echo "operation system $(uname -a)"
+. /etc/os-release
+echo "operating system $VERSION"
+echo "kernel $(uname -a)"
 echo "node $(node -v)"
 echo "yarn $(yarn -v)"
 /opt/google/chrome/google-chrome --version
