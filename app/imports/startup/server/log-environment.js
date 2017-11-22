58df9d20a707559a879ac23a96a8a92bd1f9a983
Noah Spitzer-Williams <noah@spitzer-williams.com>
2017-11-21 22:40:41 -0800

    log extra info about environment

--- app/imports/startup/server/log-environment.js
+++ app/imports/startup/server/log-environment.js
@@ -8,3 +8,6 @@ log.info('Meteor.isAppTest:', Meteor.isAppTest);
 log.info('Meteor.isDevelopment:', Meteor.isDevelopment);
 log.info('Meteor.isProduction:', Meteor.isProduction);
 
+log.info('BUNDLE_PATH:', process.env.BUNDLE_PATH);
+log.info('ROOT_URL:', process.env.ROOT_URL);
+log.info('APP_DIR', process.env.APP_DIR);
