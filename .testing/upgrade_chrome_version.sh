925ca1e1b6d90ce058e1ff2e80431f7671c40d17
Noah Spitzer-Williams <noah@spitzer-williams.com>
2018-04-29 09:00:30 -0700

    refactor

--- .testing/upgrade_chrome_version.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# https://gist.github.com/Arjeno/8564d9643f16d072a85b9c9b5a9f7de0
-curl -L -o google-chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-dpkg -i google-chrome.deb
-sed -i 's|HERE/chrome\"|HERE/chrome\" --disable-setuid-sandbox|g' /opt/google/chrome/google-chrome
-rm google-chrome.deb
