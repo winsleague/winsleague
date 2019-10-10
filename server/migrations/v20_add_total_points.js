847b3f3c09aa81cbb2633673664a3fc3ae065b74
Noah Spitzer-Williams <noah@spitzer-williams.com>
2019-10-10 06:50:54 -0700

    added migration

---
+++ server/migrations/v20_add_total_points.js
@@ -0,0 +1,32 @@
+import { Migrations } from 'meteor/percolate:migrations';
+import { PoolTeams } from '../../imports/api/pool_teams/pool_teams';
+import { PoolTeamPicks } from '../../imports/api/pool_team_picks/pool_team_picks';
+
+Migrations.add({
+  version: 20,
+  name: 'Adds totalPoints',
+  up: () => {
+    PoolTeamPicks.find().forEach((poolTeamPick) => {
+      PoolTeamPicks.direct.update(poolTeamPick._id,
+        {
+          $set: {
+            pointsMetric: 'wins',
+            totalPoints: poolTeamPick.totalWins,
+          },
+        });
+    });
+
+    PoolTeams.find().forEach((poolTeam) => {
+      PoolTeams.direct.update(poolTeam._id,
+        {
+          $set: {
+            totalPoints: poolTeam.totalWins,
+          },
+        });
+    });
+  },
+  down: () => {
+    PoolTeams.direct.update({}, { $unset: { totalPoints: '' } }, { multi: true });
+    PoolTeamPicks.direct.update({}, { $unset: { totalPoints: '', pointsMetric: '' } }, { multi: true });
+  },
+});
